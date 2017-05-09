b6bc4886fbad32d0013c4f1d1aa579f2be1fdee5
jakubkarbowski <karbon25@wp.pl>
2017-05-09 14:59:38 +0200

    FirebaseUI auth added

--- scripts/first.js
+++ scripts/first.js
@@ -1,4 +1,15 @@
-// 
-
+<script src="https://www.gstatic.com/firebasejs/3.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBemcc_sXdi1ByVcUBCKh6lWWLUWNyCGno",
+    authDomain: "family-avaiability-system.firebaseapp.com",
+    databaseURL: "https://family-avaiability-system.firebaseio.com",
+    projectId: "family-avaiability-system",
+    storageBucket: "family-avaiability-system.appspot.com",
+    messagingSenderId: "557251109841"
+  };
+  firebase.initializeApp(config);
+</script>
 
 
